befab1ddff38d8eddea9254f07ec0fd6323ea34f
Danil Lukov <severonic@gmail.com>
2020-10-12 15:39:33 +0700

    Added login form authentication

---
+++ src/DataFixtures/UserFixture.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class UserFixture extends Fixture
+{
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+    
+    public function load(ObjectManager $manager)
+    {
+        $user = new User();
+        $user->setUsername('user');
+        $user->setPassword($this->passwordEncoder->encodePassword(
+            $user,
+            'secret'
+        ));
+
+        $admin = new User();
+        $admin->setUsername('admin');
+        $admin->setRoles(['ROLE_ADMIN']);
+        $admin->setPassword($this->passwordEncoder->encodePassword(
+            $admin,
+            'secret'
+        ));
+
+        $manager->persist($user);
+        $manager->persist($admin);
+
+        $manager->flush();
+    }
+}
